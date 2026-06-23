789ca7c792291172a0ceb541f171f8c91b3e7a73
singsangsong28 <kuu052888@gmail.com>
2026-06-24 00:22:08 +0900

    feat: 자유게시판 페이지 구현 (#149)

---
+++ src/app/community/[id]/page.jsx
@@ -0,0 +1,15 @@
+import { getPostById } from "@/lib/PostAPI";
+import ArticleDetail from "./_components/ArticleDetail";
+import CommentSection from "./_components/CommentSection";
+
+export default async function CommunityDetail({ params }) {
+  const { id } = await params;
+  const post = await getPostById(id);
+
+  return (
+    <div className="page-container mt-[34px] flex justify-start flex-col">
+      <ArticleDetail post={post} />
+      <CommentSection postId={id} />
+    </div>
+  );
+}
